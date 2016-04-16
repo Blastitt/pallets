89683922c385073c6f42eb7202347ea83320c65a
relloydjr <relloydjr@gmail.com>
2016-04-16 09:47:32 -0400

    added create & edit php files

---
+++ website/edit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+<head>
+	
+</head>
+<body>
+	<h1>Pallet Finder</h1>
+	<p>Find items and pallets around the shipyard</p>
+	
+	<img src="images/app.png" />
+	<a href="create.php"> Create a Pallet</a>
+	<a href="edit.php"> Edit a Pallet</a>
+	
+	
+</body>
+</html>
