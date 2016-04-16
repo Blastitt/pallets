acbe18902b83029faeb611b26b331a973dd2d38f
relloydjr <relloydjr@gmail.com>
2016-04-16 10:09:01 -0400

    more css.

--- website/create.php
+++ website/create.php
@@ -2,14 +2,63 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/global.css">
+	<style>
+	table td{
+		
+	}
+	</style>
 </head>
 <body>
-	<h1>Pallet Finder</h1>
-	<p>Find items and pallets around the shipyard</p>
 	
-	<img src="images/app.png" />
-	<a href="create.php"> Create a Pallet</a>
-	<a href="edit.php"> Edit a Pallet</a>
+	<div id="wrapper">
+		<h1>Pallet Finder</h1>
+		<p>create pallet</p>
+	
+		<table>
+			<tr>
+				<td>
+					Pallet Name:
+				</td>
+				<td>
+					<input type="text">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Some Field:
+				</td>
+				<td>
+					<input type="text">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Some Field:
+				</td>
+				<td>
+					<input type="text">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Some Field:
+				</td>
+				<td>
+					<input type="text">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Some Field:
+				</td>
+				<td>
+					<input type="text">
+				</td>
+			</tr>
+		</table>
+		
+		<span class="ugly-hii-yellow clickable">Submit</span>
+	</div>
 	
 	
 </body>
