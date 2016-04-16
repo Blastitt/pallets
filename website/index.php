209b3121bc5d95f71b0a5fd1d5a5c650740e3be5
relloydjr <relloydjr@gmail.com>
2016-04-16 09:46:27 -0400

    updated index page

--- website/index.php
+++ website/index.php
@@ -4,6 +4,13 @@
 	
 </head>
 <body>
-	hello world 
+	<h1>Pallet Finder</h1>
+	<p>Find items and pallets around the shipyard</p>
+	
+	<img src="images/app.png" />
+	<a href="create.php"> Create a Pallet</a>
+	<a href="edit.php"> Edit a Pallet</a>
+	
+	
 </body>
 </html>
