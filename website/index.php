85b965766ed2e45b41a3a5e5ef7b0d45cc2ff08d
relloydjr <relloydjr@gmail.com>
2016-04-16 10:56:08 -0400

    find pallet page created

--- website/index.php
+++ website/index.php
@@ -27,9 +27,12 @@
 		<p>Find items and pallets around the shipyard</p>
 	
 		<!--<img src="images/app.png" /> -->
+		<a href="findpallet.php"><div class="largeButton">Find a Pallet</div></a>
 		<a href="createpallet.php">	<div class="largeButton">Create a Pallet</div></a>
 		<a href="edit.php"><div class="largeButton">Edit a Pallet</div></a>
 		<a href="createpart.php"><div class="largeButton">Create a Part</div></a>
+		
+		<!--<input type="text" style="width:100%;font-size:20px"> -->
 	</div>
 	
 	
